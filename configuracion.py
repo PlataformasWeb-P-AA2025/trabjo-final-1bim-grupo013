d68d4653ee699647a5c11d0e60de5fbdcfe0b3ce
santyago0 <santyago0515@gmail.com>
2025-05-27 22:03:10 -0500

    Base de datos en Postgres a través de Docker

--- configuracion.py
+++ configuracion.py
@@ -1,4 +1,8 @@
 # Link de enlace para la base de datos SQLite
 # creamos el conector a la base de datos
 
-cadena_base_datos = 'sqlite:///taller_final.db' 
+# cadena_base_datos = 'sqlite:///taller_final.db'
+
+# Link de enlace para base de datos en Postgres
+
+cadena_base_datos = 'postgresql+psycopg2://postgres:mypgdbpass@localhost:5432/postgres'
